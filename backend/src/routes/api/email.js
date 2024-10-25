590f3b5740188fe3dcb1ab54f267715bd76f0389
Manh Huy <levi2k3ds@gmail.com>
2024-10-25 09:52:46 +0700

    update code

--- backend/src/routes/api/email.js
+++ backend/src/routes/api/email.js
@@ -4,11 +4,11 @@ var router = express.Router();
 const emailController = require("../../controllers/api/email.controller");
 const authMiddleware = require("../../middlewares/api/auth.middleware");
 
-router.get("/", emailController.index);
-router.get("/:id", emailController.find);
-router.post("/", emailController.create);
-router.patch("/:id", emailController.update);
-router.delete("/:id", emailController.delete);
-router.delete("/", emailController.deleteAll);
+router.get("/", authMiddleware, emailController.index);
+router.get("/:id", authMiddleware, emailController.find);
+router.post("/", authMiddleware, emailController.create);
+router.patch("/:id", authMiddleware, emailController.update);
+router.delete("/:id", authMiddleware, emailController.delete);
+router.delete("/", authMiddleware, emailController.deleteAll);
 
 module.exports = router;
--- backend/src/routes/api/page.js
+++ backend/src/routes/api/page.js
@@ -4,11 +4,11 @@ var router = express.Router();
 const pageController = require("../../controllers/api/page.controller");
 const authMiddleware = require("../../middlewares/api/auth.middleware");
 
-router.get("/", pageController.index);
-router.get("/:id", pageController.find);
-router.post("/", pageController.create);
-router.patch("/:id", pageController.update);
-router.delete("/:id", pageController.delete);
-router.delete("/", pageController.deleteAll);
+router.get("/", authMiddleware, pageController.index);
+router.get("/:id", authMiddleware, pageController.find);
+router.post("/", authMiddleware, pageController.create);
+router.patch("/:id", authMiddleware, pageController.update);
+router.delete("/:id", authMiddleware, pageController.delete);
+router.delete("/", authMiddleware, pageController.deleteAll);
 
 module.exports = router;
